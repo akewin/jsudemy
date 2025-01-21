73bda5310d4e5b8710e6a0371e857cb4f0f6c0cc
Anthony Kewin <kewintk4@gmail.com>
2025-01-21 16:03:30 +0000

    new lecture
    new lecture

--- 11-Arrays-Bankist/starter/script.js
+++ 11-Arrays-Bankist/starter/script.js
@@ -60,24 +60,23 @@ const inputTransferAmount = document.querySelector('.form__input--amount');
 const inputLoanAmount = document.querySelector('.form__input--loan-amount');
 const inputCloseUsername = document.querySelector('.form__input--user');
 const inputClosePin = document.querySelector('.form__input--pin');
+/////////////////////////////////////////////////
+/////////////////////////////////////////////////
+// BANKIST APP
+
+
 
 /////////////////////////////////////////////////
 /////////////////////////////////////////////////
 // LECTURES
 
-const currencies = new Map([
-  ['USD', 'United States dollar'],
-  ['EUR', 'Euro'],
-  ['GBP', 'Pound sterling'],
-]);
-
 const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 
-for (const movement of movements) {
+for (const [i, movement] of movements.entries()) {
   if (movement > 0) {
-    console.log(`You deposited ${movement}`);
+    console.log(`Movement ${i + 1}You deposited ${movement}`);
   } else {
-    console.log(`You withdrew ${Math.abs(movement)}`);
+    console.log(`Movement ${i + 1} You withdrew ${Math.abs(movement)}`);
   }
 }
 
@@ -135,4 +134,23 @@ console.log(arr.at(-1));
 console.log('kewin'.at(0));
 */
 
-//new lecture
+// FOREACH WITH MAPS AND SETS
+console.log('---------- forEach with maps and sets ----------');
+
+//map
+const currencies = new Map([
+  ['USD', 'United States dollar'],
+  ['EUR', 'Euro'],
+  ['GBP', 'Pound sterling'],
+]);
+
+currencies.forEach(function (value, key, map) {
+  console.log(`${value}, ${key}`);
+});
+
+//set
+const currenciesUnique = new Set(['USD', 'BRL', 'GBP', 'ETH']);
+console.log(currenciesUnique);
+currenciesUnique.forEach(function (value, _, map) {
+  console.log(`${value}: ${value}`);
+});
