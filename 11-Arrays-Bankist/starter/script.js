f26e8b15c5f8f94d6ad6143f38ca6a8140d98a12
Anthony Kewin <kewintk4@gmail.com>
2025-01-22 10:34:22 +0000

    new lecture

--- 11-Arrays-Bankist/starter/script.js
+++ 11-Arrays-Bankist/starter/script.js
@@ -63,8 +63,18 @@ const inputClosePin = document.querySelector('.form__input--pin');
 /////////////////////////////////////////////////
 /////////////////////////////////////////////////
 // BANKIST APP
-
-
+const displayMovemets = function (movements) {
+  movements.forEach(function (mov, i) {
+    const type = mov > 0 ? 'deposit' : 'withdraw';
+    const html = `
+    <div class="movements__row">
+      <div class="movements__type movements__type--${type}">${i + 1} ${type}</div>
+      <div class="movements__value">${mov}€</div>
+    </div>`;
+  });
+  return html;
+};
+displayMovemets(account1.movements);
 
 /////////////////////////////////////////////////
 /////////////////////////////////////////////////
